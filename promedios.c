54b0140e611803b93dcf6f9deac5fa01fbf7ca8d
IngGiova <giovanny.padilla@udla.edu.ec>
2023-12-06 16:03:02 -0500

    creacion de variables

---
+++ promedios.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ALUMNOS 23
+#define PROGRESOS 3
+
+int main(int argc, char const *argv[])
+{
+    float notas[ALUMNOS][PROGRESOS];
+    float promedioA[ALUMNOS];
+    float gpromedio[PROGRESOS];
+    return 0;
+}
