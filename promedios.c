1de862eea2295e9d726d96543ea106cf1a35d02d
IngGiova <giovanny.padilla@udla.edu.ec>
2023-12-06 16:42:29 -0500

    impresion de los resultados

--- promedios.c
+++ promedios.c
@@ -5,40 +5,35 @@
 #define ALUMNOS 23
 #define PROGRESOS 3
 
-int main(int argc, char const *argv[])
+int main()
 {
     float notas[ALUMNOS][PROGRESOS];
     float promedioA[ALUMNOS];
     float gpromedio[PROGRESOS];
 
     srand(time(NULL));
-    
-    //Generar notas aleatorias del 1 al 10
 
+    // Generar notas aleatorias del 1 al 10
     for (int i = 0; i < ALUMNOS; i++)
     {
         for (int j = 0; j < PROGRESOS; j++)
         {
             notas[i][j] = rand() % 10 + 1;
         }
-        
     }
 
     // Calcular promedio de cada alumno
-
     for (int i = 0; i < ALUMNOS; i++)
     {
-        float suma=0;
-        for (int j = 0; j < PROGRESOS ; i++)
+        float suma = 0;
+        for (int j = 0; j < PROGRESOS; j++)
         {
-            promedioA[i] = suma/PROGRESOS;
-
+            suma += notas[i][j];
         }
-        
+        promedioA[i] = suma/PROGRESOS;
     }
 
-    //calculo de promedio del grupo por cada progreso
-
+    // Calcular promedio del grupo por cada progreso
     for (int j = 0; j < PROGRESOS; j++)
     {
         float suma = 0;
@@ -46,21 +41,33 @@ int main(int argc, char const *argv[])
         {
             suma += notas[i][j];
         }
-    gpromedio[j]= suma/ALUMNOS;
-
+        gpromedio[j] = suma/ALUMNOS;
     }
-    
-    //encontrar al alumno con mejor promedio 
+
+    // Encontrar al alumno con mejor promedio
     int Malumno = 0;
     for (int i = 1; i < ALUMNOS; i++)
     {
-        if (promedioA[i] > promedioA[Malumno]) {
+        if (promedioA[i] > promedioA[Malumno])
+        {
             Malumno = i;
         }
     }
 
-      
-    
+    // Imprimir los resultados
+    printf("\nResultados\n");
+
+    for (int i = 0; i < ALUMNOS; i++)
+    {
+        printf("Promedio del Alumno %d: %.2f\n", i + 1, promedioA[i]);
+    }
+
+    for (int j = 0; j < PROGRESOS; j++)
+    {
+        printf("Promedio del grupo en el progreso %d: %.2f\n", j + 1, gpromedio[j]);
+    }
+
+    printf("Mejor alumno: %d, Promedio: %.2f\n", Malumno + 1, promedioA[Malumno]);
 
     return 0;
 }
