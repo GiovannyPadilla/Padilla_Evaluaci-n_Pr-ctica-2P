8a840d842108a123b42d2e51ccf1caef15c87c54
IngGiova <giovanny.padilla@udla.edu.ec>
2023-12-06 16:07:14 -0500

    uso del for para generar notas del 1 al 10

--- promedios.c
+++ promedios.c
@@ -10,5 +10,20 @@ int main(int argc, char const *argv[])
     float notas[ALUMNOS][PROGRESOS];
     float promedioA[ALUMNOS];
     float gpromedio[PROGRESOS];
+
+    srand(time(NULL));
+    
+    //Generar notas aleatorias del 1 al 10
+
+    for (int i = 0; i < ALUMNOS; i++)
+    {
+        for (int j = 0; j < PROGRESOS; j++)
+        {
+            notas[i][j] = rand() % 10 + 1;
+        }
+        
+    }
+    
+
     return 0;
 }
